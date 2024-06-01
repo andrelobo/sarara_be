d07964844267efaffdc8c4fc222f0e8093cca411
Andre Lobo <loboandre@hotmail.com>
2024-06-01 14:22:42 -0400

    feat:BeverageHistory component

--- app.js
+++ app.js
@@ -82,7 +82,7 @@ app.get('/api/beverages/:id', beverageController.getBeverageById);
 app.put('/api/beverages/:id', beverageController.updateBeverage);
 app.delete('/api/beverages/:id', beverageController.deleteBeverage);
 
-app.get('/:id/history', beverageController.getBeverageHistoryByDate);
+app.get('/api/beverages/:id/history', beverageController.getBeverageHistoryByDate);
 //Rotas para Ingredientes
 
 app.post('/api/ingredients', ingredientController.createIngredient);
