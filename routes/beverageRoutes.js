87551d82fea1b749b2a22527c999855030771f76
Andre Lobo <loboandre@hotmail.com>
2024-07-10 15:16:27 -0400

    fix: open routes to test

--- routes/beverageRoutes.js
+++ routes/beverageRoutes.js
@@ -1,26 +1,26 @@
 const express = require('express');
 const router = express.Router();
 const beverageController = require('../controllers/beverageController');
-const authenticateToken = require('../middlewares/authenticateToken');
+// const authenticateToken = require('../middlewares/authenticateToken');
 
 // Rotas para bebidas
-router.post('/', authenticateToken, beverageController.createBeverage);
-router.get('/', authenticateToken, beverageController.getAllBeverages);
-router.get('/:id', authenticateToken, (req, res, next) => {
+router.post('/', beverageController.createBeverage);
+router.get('/', beverageController.getAllBeverages);
+router.get('/:id',(req, res, next) => {
   const { id } = req.params;
   if (!id) {
     return res.status(400).json({ error: 'Missing id parameter' });
   }
   beverageController.getBeverageById(req, res, next);
 });
-router.put('/:id', authenticateToken, (req, res, next) => {
+router.put('/:id', (req, res, next) => {
   const { id } = req.params;
   if (!id) {
     return res.status(400).json({ error: 'Missing id parameter' });
   }
   beverageController.updateBeverage(req, res, next);
 });
-router.delete('/:id', authenticateToken, (req, res, next) => {
+router.delete('/:id', (req, res, next) => {
   const { id } = req.params;
   if (!id) {
     return res.status(400).json({ error: 'Missing id parameter' });
