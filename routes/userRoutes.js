4b55c1d469cbb3f7190d6f3a3e94ea6acc4baf3c
André Lobo <loboandre@hotmail.com>
2024-11-01 16:09:15 -0400

    test: noa uth just for test purposes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,24 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const UserController = require('../controllers/userController');
-const authenticateToken = require('../middlewares/authenticateToken');
+//const authenticateToken = require('../middlewares/authenticateToken');
 
 // Rota para criar um novo usuário
-router.post('/', authenticateToken, UserController.createUser);
+router.post('/', UserController.createUser);
 
 // Rota para autenticar um usuário
 router.post('/login', UserController.loginUser);
 
 // Rota para obter detalhes de um usuário pelo ID
-router.get('/:id', authenticateToken, UserController.getUserById);
+router.get('/:id', authenticate, UserController.getUserById);
 
 // Rota para atualizar os detalhes de um usuário pelo ID
-router.put('/:id', authenticateToken, UserController.updateUserById);
+router.put('/:id',  UserController.updateUserById);
 
 // Rota para excluir um usuário pelo ID
-router.delete('/:id', authenticateToken , UserController.deleteUserById);
+router.delete('/:id', UserController.deleteUserById);
 
 // Rota para listar todos os usuários
-router.get('/', authenticateToken , UserController.getAllUsers);
+router.get('/',  UserController.getAllUsers);
 
 module.exports = router;
