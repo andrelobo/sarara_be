47da798d06d060f3638b39ff3d00d26b91fc63cc
André Lobo <loboandre@hotmail.com>
2024-11-01 09:36:19 -0400

    fix: testing purposes

--- routes/beverageRoutes.js
+++ routes/beverageRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const beverageController = require('../controllers/beverageController');
-const authenticateToken = require('../middlewares/authenticateToken');
+//const authenticateToken = require('../middlewares/authenticateToken');
 
 // Middleware para verificar a presença do ID no request params
 const checkIdParam = (req, res, next) => {
@@ -13,15 +13,15 @@ const checkIdParam = (req, res, next) => {
 };
 
 // Rotas para bebidas
-router.post('/',authenticateToken, beverageController.createBeverage);
-router.get('/',authenticateToken,  beverageController.getAllBeverages);
-router.get('/:id', checkIdParam, authenticateToken,  beverageController.getBeverageById);
-router.put('/:id', checkIdParam, authenticateToken,  beverageController.updateBeverage);
-router.delete('/:id', checkIdParam, authenticateToken, beverageController.deleteBeverage);
-router.post('/:id/history', checkIdParam, authenticateToken, beverageController.getBeverageHistory);
+router.post('/', beverageController.createBeverage);
+router.get('/',  beverageController.getAllBeverages);
+router.get('/:id', checkIdParam, beverageController.getBeverageById);
+router.put('/:id', checkIdParam,  beverageController.updateBeverage);
+router.delete('/:id', checkIdParam, beverageController.deleteBeverage);
+router.post('/:id/history', checkIdParam, beverageController.getBeverageHistory);
 
 // Novas rotas para gráficos
-router.get('/graphs/most-least-sold',authenticateToken, beverageController.getMostLeastSoldBeverages);
+router.get('/graphs/most-least-sold', beverageController.getMostLeastSoldBeverages);
 router.get('/graphs/never-sold',authenticateToken,  beverageController.getNeverSoldBeverages);
 router.get('/graphs/change-history',authenticateToken,  beverageController.getChangeHistory);
 
